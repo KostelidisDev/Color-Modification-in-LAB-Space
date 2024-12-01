902bd3d96b0bcbc259b5863b184955c90b2ef297
Iordanis Kostelidis <ikostelidis@kostelidis.dev>
2024-12-01 18:24:59 +0200

    Calculate image dimensions and aspect ratio

--- Code.m
+++ Code.m
@@ -10,3 +10,7 @@ target_image_height = 500;
 % Main Script
 input_image = imread(input_image_file);
 clear input_image_file;
+
+[image_height, image_width, ~] = size(input_image);
+image_aspect_ratio = image_width / image_height;
+clear image_width image_height image_depth;
