5e0851cc3fd27e0ea25e4256d01f4f947b3d354c
Iordanis Kostelidis <ikostelidis@kostelidis.dev>
2024-12-01 18:25:27 +0200

    Calculate target dimensions

--- Code.m
+++ Code.m
@@ -14,3 +14,6 @@ clear input_image_file;
 [image_height, image_width, ~] = size(input_image);
 image_aspect_ratio = image_width / image_height;
 clear image_width image_height image_depth;
+
+target_image_width = target_image_height * image_aspect_ratio;
+clear image_aspect_ratio;
